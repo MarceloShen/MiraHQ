d28ae004689656fe8d2827acec64a4f663449010
MarceloShen <s-shenc@bsd405.org>
2020-09-18 15:31:15 -0700

    finished homework

--- Group_Work/Assignments/LLQueue.java
+++ Group_Work/Assignments/LLQueue.java
@@ -1,3 +1,5 @@
+// import jdk.jfr.internal.OldObjectSample;
+
 /*
  * This class implements a queue with linked list
  * Author: Meng Yang
@@ -13,59 +15,89 @@ public class LLQueue {
 
         // Constructor with no parameters for inner class
         public Node() {
-           
+
         }
 
         // Parametrized constructor for inner class
         public Node(Object newData, Node nextLink) {
-            // to do: Data part of Node is an Object
-            // to do: Link to next node is a type Node
+            data = newData; // TODO: Data part of Node is an Object
+            next = nextLink;// TODO: Link to next node is a type Node
         }
     }
-    
+
     private Node front;
     private Node back;
 
-    public LLQueueForStudents() {
-     // to do
+    public LLQueue() {
+        // LinkedList
+        // TODO
     }
-    
-    //offer(enqueue) adds the object at the back of the queue
+
+    // offer(enqueue) adds the object at the back of the queue
+    /**
+     * adds the object at the back of the queue
+     * 
+     * @param o object to be added
+     */
     public void offer(Object o) {
-     // to do
+        if (back == null) {
+            back = new Node(o, null);
+            front = back;
+        } else {
+            back.next = new Node(o, null);
+            back = back.next;
+        }
+        // TODO
     }
-    
-    //poll(dequeue): retrieves and removes the head of this queue, 
-    //or returns null if this queue is empty.
+
+    // poll(dequeue): retrieves and removes the head of this queue,
+    // or returns null if this queue is empty.
     public Object poll() {
-     // to do
+        if (front != null) {
+            Node result = front;
+            front = front.next;
+            return result;
+        } else {
+            return null;
+        }
+        // TODO
     }
-    
+
     // Returns the size of linked list by traversing the list
     public int size() {
-        // to do
+        int counter = 0;
+        Node iterator = front;
+        while (iterator != null) {
+            iterator = iterator.next;
+            counter++;
+        }
+        return counter;
+        // TODO
     }
-    //peek: Retrieves, but does not remove, the head of this queue, 
-    //or returns null if this queue is empty.
+
+    // peek: Retrieves, but does not remove, the head of this queue,
+    // or returns null if this queue is empty.
     public Object peek() {
-        // to do
-    } 
-    
+        return front;
+        // TODO
+    }
+
     //
     public boolean isEmpty() {
-        // to do
-    } 
-    
+        return front == null;
+        // TODO
+    }
+
     // For two lists to be equal they must contain the same data items in
     // the same order. The equals method of T is used to compare data items.
     public boolean equals(Object otherObject) {
         if (otherObject == null)
             return false;
 
-        else if (!(otherObject instanceof LLQueueForStudents)) {
+        else if (!(otherObject instanceof LLQueue)) {
             return false;
         } else {
-            LLQueueForStudents otherList = (LLQueueForStudents) otherObject;
+            LLQueue otherList = (LLQueue) otherObject;
             if (size() != otherList.size())
                 return false;
             Node position = front;
@@ -79,14 +111,14 @@ public class LLQueue {
             return true; // objects are the same
         }
     }
-    
+
     // There is no need to modify the driver
     public static void main(String[] args) {
-     // input data for testing
+        // input data for testing
         String target = "Somethings!";
         String palindrome = "a man a plan canal panama";
 
-        LLQueueForStudents list = new LLQueueForStudents();
+        LLQueue list = new LLQueue();
         // objects to be added to list
         Object object1 = (Character) target.charAt(4);
         Object object2 = (Character) target.charAt(1);
@@ -99,37 +131,35 @@ public class LLQueue {
         list.offer(object2);
         list.offer(object3);
         list.offer(object4);
-        
+
         // make sure all are added
         System.out.println("My list has " + list.size() + " nodes.");
-        
-        //testing equals
-        LLQueueForStudents list2 = new LLQueueForStudents();
+
+        // testing equals
+        LLQueue list2 = new LLQueue();
         // add 4 objects to the new linked list
-        list2.offer(object1);//t
-        list2.offer(object2);//o
-        list2.offer(object3);//m
-        list2.offer(object4);//s
+        list2.offer(object1);// t
+        list2.offer(object2);// o
+        list2.offer(object3);// m
+        list2.offer(object4);// s
         boolean isEqual2 = list.equals(list2);
         System.out.println("list2 is equal to list1? " + isEqual2);
-        
+
         // add 4 objects to our linked list in a different order
-        LLQueueForStudents list3 = new LLQueueForStudents();
-        list3.offer(object3);//m
-        list3.offer(object1);//t
-        list3.offer(object2);//o
-        list3.offer(object4);//s
+        LLQueue list3 = new LLQueue();
+        list3.offer(object3);// m
+        list3.offer(object1);// t
+        list3.offer(object2);// o
+        list3.offer(object4);// s
         boolean isEqual3 = list.equals(list3);
         System.out.println("list3 is equal to list1? " + isEqual3);
-        
+
         // testing isEmpty() and poll()
-        while(!list.isEmpty()) {
+        while (!list.isEmpty()) {
             Object temp = list.poll();
             System.out.println("Polling " + temp);
         }
-       
-    }
 
-   
+    }
 
 }
