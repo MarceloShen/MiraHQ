9f246b54d34fb0420a82f43401cc6ff4c9eae4a9
s-yib <55288448+s-yib@users.noreply.github.com>
2020-09-22 16:19:53 -0700

    added stack implementation for suitors

--- Group_Work/Assignments/UsingStacksSuitorsLab.java
+++ Group_Work/Assignments/UsingStacksSuitorsLab.java
@@ -10,7 +10,7 @@ import java.util.Stack;
  * iteratively and recursively. The UsingStacksSuitorsLab class itself is a
  * runnable object, so it can be passed to a thread in our Queue demo.
  * 
- * @author Caogang Shen, Ashley Mead, Brandon Yi
+ * @author Caogang Shen, Ashley Mead, Brandon Yi, Eashver Elango
  * 
  */
 
@@ -106,6 +106,8 @@ public class UsingStacksSuitorsLab implements Runnable {
 	}
 
 	public static int findPlaceToStand(int numSuitors) {
+		// queue implementation
+		/*
 		Queue<Integer> line = new LinkedList<Integer>();
 		for (int i = 1; i <= numSuitors; i++) {
 			line.offer(i);
@@ -121,8 +123,40 @@ public class UsingStacksSuitorsLab implements Runnable {
 		}
 		// TODO
 		return line.poll();
+		*/
+		
+		//stack implementation
+		Stack<Integer> line = new Stack<Integer>();
+		Stack<Integer> line2 = new Stack<Integer>();
+		for (int i = numSuitors; i > 0; i--) {
+			line.push(i);
+		}
+		int count = 0;
+		while (line.size()+line2.size() > 1) {
+			while (line.size() > 0) {
+				count++;
+				if (count % 3 == 0) {
+					System.out.println(line.pop());
+				} else {
+					line2.push(line.pop());
+				}
+			}
+			while (line2.size() > 0) {
+				count++;
+				if (count % 3 == 0) {
+					System.out.println(line2.pop());
+				} else {
+					line.push(line2.pop());
+				}
+			}
+		}
+		if (line.size() > 0) {
+			return line.peek();
+		} else {
+			return line2.peek();
+		}
 	}
-
+	
 	public static boolean buildThreadQueue() { // returns true upon success
 		Queue<Thread> q = new LinkedList<Thread>();
 
@@ -158,7 +192,7 @@ public class UsingStacksSuitorsLab implements Runnable {
 
 	@Override
 	public void run() {
-		for (int i = 0; i < 1000; i++) {
+		for (int i = 0; i < 1; i++) {
 			System.out.println(name + ": " + i + "th iteration");
 			try {
 				Thread.sleep(10);
