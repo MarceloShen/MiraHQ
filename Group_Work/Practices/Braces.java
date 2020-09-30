780b1f965beedd0992147e15c2edd9a260a77466
MarceloShen <s-shenc@bsd405.org>
2020-09-30 15:39:13 -0600

    Keep it safe

--- Group_Work/Practices/Braces.java
+++ Group_Work/Practices/Braces.java
@@ -1,19 +1,43 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Stack;
 
+/**
+ * This class serves to check whether braces in an array of strings are balanced
+ * 
+ * @author Marcelo Shen, Ashley Mead, Brandon Yi, Eashver Elango
+ */
 public class Braces {
 
+    /**
+     * Test example cases
+     * 
+     * @param args do nothing
+     */
     public static void main(String[] args) {
-        String[] test1 = { "{}[]()" };
+        String[] test1 = { "{}[]()", "()())()()()()()))(()(", "{[()]}", "{]}()" };
         String[] result1 = braces(test1);
-        System.out.println(result1[0]);
+        for (int i = 0; i < result1.length; i++) {
+            System.out.println(result1[i]);
+        }
+
     }
 
     private static Map<Character, Character> matches;
 
-    // Complete the braces function below.
-    static String[] braces(String[] values) {
-        matches = new HashMap<Character, Character>();
+    /**
+     * Checks whether each string in an array of strings has balanced left and right
+     * braces by disassembling the array to check each string in checkMatching()
+     * method
+     * 
+     * @param values An array of strings where each string contains a series of left
+     *               and right braces
+     * @return An array of strings that indicates whether in each case the braces in
+     *         the string are balanced
+     * 
+     */
+    public static String[] braces(String[] values) {
+        matches = new HashMap<>();
         matches.put('(', ')');
         matches.put('[', ']');
         matches.put('{', '}');
@@ -25,23 +49,29 @@ public class Braces {
         return result;
     }
 
+    /**
+     * Checks whether a string has balanced left and right braces. Iterate through
+     * the string; whenever it reaches a left braces, the program adds the braces to
+     * a stack; otherwise it checks whether the last left braces in the stack
+     * matches the right braces. If there is nothing left in the stack after
+     * iterating through the string, return true; otherwise return false.
+     *
+     * 
+     * @param current String containing a series of left and right braces
+     * @return Whether the braces in the string are balanced
+     */
     private static boolean checkMatching(String current) {
-        StringBuilder lefts = new StringBuilder();
+        Stack<Character> lefts = new Stack<>();
         for (int i = 0; i < current.length(); i++) {
             char brace = current.charAt(i);
             if (matches.containsKey(brace)) {
-                lefts.append(brace);
+                lefts.push(brace);
             } else {
-                if (lefts.length() == 0) {
+                if (lefts.isEmpty() || matches.get(lefts.pop()) != brace) {
                     return false;
-                }
-                if (!(matches.get(lefts.charAt(lefts.length() - 1)) == brace)) {
-                    return false;
-                } else {
-                    lefts.deleteCharAt(lefts.length() - 1);
                 }
             }
         }
-        return lefts.length() == 0;
+        return lefts.isEmpty();
     }
 }
