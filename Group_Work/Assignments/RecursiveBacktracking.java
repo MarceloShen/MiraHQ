870131ad3eb21ce63fe2836b33d4a01a6af67a39
Eashver Elango <eashvere@gmail.com>
2020-10-23 14:33:26 -0700

    Updated Recursive Backtracking

--- Group_Work/Assignments/RecursiveBacktracking.java
+++ Group_Work/Assignments/RecursiveBacktracking.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 /**
  * Recursive Backtracking Example Methods
  * 
- * @author Marcelo Shen, Ashley Mead, Eashver Elango, (Brandon Yi) 
+ * @author Marcelo Shen, Ashley Mead, Eashver Elango, Brandon Yi
  */
 public class RecursiveBacktracking {
 
@@ -28,8 +28,16 @@ public class RecursiveBacktracking {
 
     /**
      * This method prints out all the possible routes to go to (x, y) from the
-     * origin. Calls the travelHelper() method with given params. O(n) space; O(2^n)
-     * time.
+     * origin. Calls the travelHelper() method with given params. 
+     *  O(x + y) space;  O(3^(x+y)) time.
+     * 
+     *                   _____________(0, 0)_______________
+     *                 /                 |                 \
+     *              _(1, 0)_          _(0, 1)_         ___(1, 1)__
+     *             /   |    \        /    |   \       /     |     \
+     *         (2, 0)(1, 1)(2, 1)(1, 1)(0, 2)(1, 1)(2, 1)(1, 2)(2, 2)
+     *                                  ...
+     * 
      * 
      * @param x horizontal distance to move
      * @param y vertical distance to move
@@ -46,6 +54,7 @@ public class RecursiveBacktracking {
      * @param y             vertical distance to move
      * @param currentString the current route, using N - North, E - East, NE -
      *                      Northeast
+     * 
      */
     private static void travelHelper(int x, int y, StringBuilder currentString) {
         // base case
@@ -92,6 +101,16 @@ public class RecursiveBacktracking {
      * with the given params. O(n) space; O(2^n) time.
      * 
      * @param n the number of digits for each binary number
+     * 
+     * 
+     *                              _______""________
+     *                             /                 \
+     *                            0                   1
+     *                        /       \           /       \
+     *                       00       01         10       11
+     *                      /  \     /  \       /  \     /  \
+     *                     000 001  010 001   100  101  110 111
+     *                                     ...
      */
     public static void countBinary(int n) {
         StringBuilder currentString = new StringBuilder();
@@ -131,7 +150,20 @@ public class RecursiveBacktracking {
     /**
      * This method returns an integer that indicates the maximum sum from the list
      * of integers in L without exceeding the limit of n. Calls the maxSumHelper
-     * method with the given params. O(n) space, O(2^n) time.
+     * method with the given params. O(n) space, O(2^n) * n time.
+     * 
+     * Example tree:
+     * L = [10, 20, 30], n = 20
+     * 
+     *                      0
+     *             ________/ \__________
+     * 10        10                    0
+     *       ___/ \___             ___/ \___
+     * 20   X         10         20        0
+     *               / \        / \       / \
+     * 30           X  10      X  20     X   0
+     *                    ...
+     * 
      * 
      * @param L List of integers
      * @param n Limit that the maximum sum cannot exceed
@@ -188,7 +220,17 @@ public class RecursiveBacktracking {
      * @param sum1  First sublist sum
      * @param sum2  Second sublist sum
      * @return whether the List L can be separated into two equally summed sublists
-     */
+     * 
+     * Example partitionable([1, 2, 3, 4, 5, 6], 0, 0, 0)
+     *                              ______(0,0)_______
+     *                             /                  \
+     *                       (1,0)                    (0,1)
+     *                     /       \               /        \
+     *                  (3,0)      (1,2)        (2,1)      (0,3)
+     *                 /     \    /     \       /   \      /   \
+     *              (6,0) (3,3) (4,2) (1,5) (5,1) (2,4) (3,3) (0,6)
+     * ...
+     */                 
     private static boolean partitionableHelper(List<Integer> L, int index, int sum1, int sum2) {
         if (index == L.size()) {
             return sum1 == sum2; // Base Case at the end of the list, check if the sums are equal
