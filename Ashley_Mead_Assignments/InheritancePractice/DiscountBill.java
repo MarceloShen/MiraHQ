249be83ed7d1a7b0045c5d617de3268b25b83b62
s-meada <atmead03@gmail.com>
2020-11-20 21:59:52 -0800

    Comment Out Code to Get Rid of Errors

--- Ashley_Mead_Assignments/InheritancePractice/DiscountBill.java
+++ Ashley_Mead_Assignments/InheritancePractice/DiscountBill.java
@@ -1,43 +1,43 @@
-package InheritancePractice;
+// package InheritancePractice;
 
-// This class can access the super class and other objects in practice-it
-public class DiscountBill extends GroceryBill {
-    boolean preferred;
-    // Total discount amount
-    double discountsSum;
-    // Number of discounts
-    int discountCount;
+// // This class can access the super class and other objects in practice-it
+// public class DiscountBill extends GroceryBill {
+//     boolean preferred;
+//     // Total discount amount
+//     double discountsSum;
+//     // Number of discounts
+//     int discountCount;
     
-    public DiscountBill(Employee clerk, boolean preferred) {
-        super(clerk);
-        this.preferred = preferred;
-        this.discountsSum = 0.0;
-        this.discountCount = 0;
-    }
+//     public DiscountBill(Employee clerk, boolean preferred) {
+//         super(clerk);
+//         this.preferred = preferred;
+//         this.discountsSum = 0.0;
+//         this.discountCount = 0;
+//     }
     
-    public int getDiscountCount() {
-        return this.discountCount;
-    }
+//     public int getDiscountCount() {
+//         return this.discountCount;
+//     }
     
-    public double getDiscountAmount() {
-        return this.discountsSum;
-    }
+//     public double getDiscountAmount() {
+//         return this.discountsSum;
+//     }
     
-    public double getDiscountPercent() {
-        return this.getDiscountAmount() / super.getTotal() * 100;
-    }
+//     public double getDiscountPercent() {
+//         return this.getDiscountAmount() / super.getTotal() * 100;
+//     }
     
-    public double getTotal() {
-        return super.getTotal() - this.getDiscountAmount();
-    }
+//     public double getTotal() {
+//         return super.getTotal() - this.getDiscountAmount();
+//     }
     
-    public void add(Item i) {
-        super.add(i);
-        double discount = i.getDiscount();
-        // Add the discount if the customer is preferred and the discount is not 0
-        if(preferred && discount != 0.0) {
-            this.discountsSum += discount;
-            this.discountCount++;
-        }
-    }
-}
+//     public void add(Item i) {
+//         super.add(i);
+//         double discount = i.getDiscount();
+//         // Add the discount if the customer is preferred and the discount is not 0
+//         if(preferred && discount != 0.0) {
+//             this.discountsSum += discount;
+//             this.discountCount++;
+//         }
+//     }
+// }
