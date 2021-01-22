cfe6b43819b4e19dc270a9a1e9bc6d6babfee56d
MarceloShen <s-shenc@bsd405.org>
2021-01-21 22:04:41 -0800

    edit

--- Caogang_Shen(CallMeMarcelo)/assignments/ST_7_BinaryTreePractice_Caogang_Shen.java
+++ Caogang_Shen(CallMeMarcelo)/assignments/ST_7_BinaryTreePractice_Caogang_Shen.java
@@ -1,3 +1,24 @@
+/* Binary Tree Properties
+ * 3
+ * 3
+ * 2, 3, 5
+ * 3
+ * 1, 4, 6
+ * 3
+ * 5
+ * 4, 6
+*/
+/* Traversal1
+ * 3, 5, 1, 2, 4, 6
+ * 1, 5, 3, 4, 2, 6
+ * 1, 5, 4, 6, 2, 3
+*/
+/* bsdAdd1
+ * Leia, Boba, Darth, Chewy, Han, Jabba, R2D2, Luke
+ * Boba, Chewy, Darth, Han, Jabba, Leia, Luke, R2D2
+ * Chewy, Jabba, Han, Darth, Boba, Luke, R2D2, Leia 
+*/
+// size
 public class ST_7_BinaryTreePractice_Caogang_Shen {
     public class IntTree {
         private IntTreeNode overallRoot;
